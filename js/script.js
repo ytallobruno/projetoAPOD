bc8ba46b4aab0a5be4563cc56c7b4136cdbb2431
Trabalho Yta <ytallobruno@hotmail.com>
2022-02-01 16:48:24 -0300

    FINALLY fixed script

--- js/script.js
+++ js/script.js
@@ -1,15 +1,43 @@
-$(".sect-btn").on("click", async function (event) {
+$(".sect-btn").on("click", function (event) {
   event.preventDefault();
+  apod();
+});
 
+function apod() {
+  const artigo = $(".artc")
   const data = $("#date").val();
+  const ttl = $(".artc-ttl__nasa");
+  const dscc = $(".artc-txt__nasa");
+  const img = $(".artc-img__nasa");
+  const vdo = $(".artc-vd__nasa");
 
   $.ajax({
     url: `https://api.nasa.gov/planetary/apod?api_key=fPYCd2Epp6jDc2uLnSnZfG1RM9Ed4q4W2YpCbxaz&date=${data}`,
-    success: function () {
-      
+
+    success: function (search) {
+      artigo.css("display", "flex");
+      ttl.text(search.title);
+      dscc.text(search.explanation);
+
+      if (search.media_type == "image") {
+        img.attr("src", search.url);
+        img.css("display", "block");
+        vdo.css("display", "none");
+      } else {
+        vdo.attr("src", search.url);
+        img.css("display", "none");
+        vdo.css("display", "block");
+      }
+      return pesquisa;
     },
+
     error: function () {
-      
+      artigo.css("display", "flex");
+      ttl.text(`Erro na Api`);
+      dscc.text(`Por favor, insira uma data de 16 de junho de 1995 até a data atual (exceto 17, 18 e 19 de junho de 1995.`);
+      img.attr("src","./image/findSerch.png");
+      img.css("width", "150px")
+      vdo.css("display", "none");
     },
   });
-});
+}
